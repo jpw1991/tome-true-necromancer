60202ec335f20ccfadb7fe6125250172ded19749
Joshua Woods <jpw1991@gmail.com>
2016-05-02 18:55:33 +0200

    initial

---
+++ tome-better-summoning-for-necromancy/init.lua
@@ -0,0 +1,18 @@
+-- Better Summoning for Necromancy
+-- tome-better-summoning-for-necromancy/init.lua
+
+long_name = "Better Summoning for Necromancy"
+short_name = "bettersummoningfornecromancy" -- Determines the name of your addon's file.
+for_module = "tome"
+version = {1,4,6}
+addon_version = {1,0,0}
+weight = 100 -- The lower this value, the sooner your addon will load compared to other addons.
+author = {'jpw1991@gmail.com'}
+homepage = 'none'
+description = [[Adds a new necromancer class with better summoning.]]
+tags = {'better', 'summoning', 'necromancy', 'necromancer'} -- tags MUST immediately follow description
+
+overload = false
+superload = true
+data = true
+hooks = false--true
