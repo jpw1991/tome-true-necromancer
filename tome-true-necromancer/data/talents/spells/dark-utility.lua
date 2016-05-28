b31e1268f1d8cad48c5d78bf26da769a7c1e40d6
Joshua Woods <jpw1991@gmail.com>
2016-05-28 14:42:55 +0200

    swapped position of considerate minions

--- tome-true-necromancer/data/talents/spells/dark-utility.lua
+++ tome-true-necromancer/data/talents/spells/dark-utility.lua
@@ -17,10 +17,25 @@
 -- Nicolas Casalini "DarkGod"
 -- darkgod@te4.org
 
-newTalent {
-	name = "Dark Recall",
+newTalent{
+	name = "Considerate Minions",
 	type = {"spell/dark-utility",1},
 	require = spells_req_high1,
+	points = 5,
+	mode = "passive",
+	getPerc = function(self, t) return self:combatTalentSpellDamage(t, 15, 80) end,
+	info = function(self, t)
+		return ([[You share your powers with your minions, granting them %d%% of your resistances and saves.
+		In addition all damage done by your minions to you and themselves is reduced by %d%%.
+		The effect will increase with your Spellpower.]]):
+		format(t.getPerc(self, t), self:getTalentLevelRaw(t) * 20)
+	end,
+}
+
+newTalent {
+	name = "Dark Recall",
+	type = {"spell/dark-utility",2},
+	require = spells_req_high2,
 	points = 5,
 	random_ego = "attack",
 	mana = 25,
@@ -71,41 +86,6 @@ newTalent {
 }
 
 newTalent {
-	name = "Detonate Minion",
-	type = {"spell/dark-utility",2},
-	require = spells_req_high2,
-	points = 5,
-	mana = 30,
-	cooldown = 10,
-	tactical = { ATTACKAREA = { BLIGHT = 2 } },
-	radius = function(self, t) return math.floor(self:combatTalentScale(t, 1.3, 2.7)) end,
-	range = 8,
-	requires_target = true,
-	no_npc_use = true,
-	getDamage = function(self, t) return self:combatLimit(self:combatTalentSpellDamage(t, 20, 70), 90, 0, 0, 25, 25) end, -- Limit to 50% life
-	action = function(self, t)
-		local tg = {type="hit", range=self:getTalentRange(t), talent=t, first_target="friend"}
-		local tx, ty, target = self:getTarget(tg)
-		if not tx or not ty or not target or not target.summoner or not target.summoner == self or not target.necrotic_minion then return nil end
-
-		local tg = {type="ball", range=self:getTalentRange(t), radius=self:getTalentRadius(t), talent=t, friendlyfire=not rng.percent(self:getTalentLevelRaw(self.T_CONSIDERATE_MINIONS) * 0.2)}
-		local dam = target.max_life * t.getDamage(self, t) / 100
-		target:die()
-		target:project(tg, target.x, target.y, DamageType.BLIGHT, dam)
-		game.level.map:particleEmitter(target.x, target.y, tg.radius, "ball_acid", {radius=tg.radius})
-
-		game:playSoundNear(self, "talents/fireflash")
-		return true
-	end,
-	info = function(self, t)
-		return ([[Minions are only tools. You may dispose of them... Violently.
-		Makes the targeted minion explode for %d%% of its maximum life in a radius of %d as blight damage.
-		Beware! Don't get caught in the blast! (unless you know Dark Empthy: %d%% chance to ignore damage)]]):
-		format(t.getDamage(self, t),t.radius(self,t), self:getTalentLevelRaw(self.T_CONSIDERATE_MINIONS) * 20)
-	end,
-}
-
-newTalent {
 	name = "Necrotic Frenzy",
 	type = {"spell/dark-utility",3},
 	require = spells_req_high3,
@@ -141,17 +121,37 @@ newTalent {
 	end,
 }
 
-newTalent{
-	name = "Considerate Minions",
+newTalent {
+	name = "Detonate Minion",
 	type = {"spell/dark-utility",4},
 	require = spells_req_high4,
 	points = 5,
-	mode = "passive",
-	getPerc = function(self, t) return self:combatTalentSpellDamage(t, 15, 80) end,
+	mana = 30,
+	cooldown = 10,
+	tactical = { ATTACKAREA = { BLIGHT = 2 } },
+	radius = function(self, t) return math.floor(self:combatTalentScale(t, 1.3, 2.7)) end,
+	range = 8,
+	requires_target = true,
+	no_npc_use = true,
+	getDamage = function(self, t) return self:combatLimit(self:combatTalentSpellDamage(t, 20, 70), 90, 0, 0, 25, 25) end, -- Limit to 50% life
+	action = function(self, t)
+		local tg = {type="hit", range=self:getTalentRange(t), talent=t, first_target="friend"}
+		local tx, ty, target = self:getTarget(tg)
+		if not tx or not ty or not target or not target.summoner or not target.summoner == self or not target.necrotic_minion then return nil end
+
+		local tg = {type="ball", range=self:getTalentRange(t), radius=self:getTalentRadius(t), talent=t, friendlyfire=not rng.percent(self:getTalentLevelRaw(self.T_CONSIDERATE_MINIONS) * 0.2)}
+		local dam = target.max_life * t.getDamage(self, t) / 100
+		target:die()
+		target:project(tg, target.x, target.y, DamageType.BLIGHT, dam)
+		game.level.map:particleEmitter(target.x, target.y, tg.radius, "ball_acid", {radius=tg.radius})
+
+		game:playSoundNear(self, "talents/fireflash")
+		return true
+	end,
 	info = function(self, t)
-		return ([[You share your powers with your minions, granting them %d%% of your resistances and saves.
-		In addition all damage done by your minions to you and themselves is reduced by %d%%.
-		The effect will increase with your Spellpower.]]):
-		format(t.getPerc(self, t), self:getTalentLevelRaw(t) * 20)
+		return ([[Minions are only tools. You may dispose of them... Violently.
+		Makes the targeted minion explode for %d%% of its maximum life in a radius of %d as blight damage.
+		Beware! Don't get caught in the blast! (unless you know Dark Empthy: %d%% chance to ignore damage)]]):
+		format(t.getDamage(self, t),t.radius(self,t), self:getTalentLevelRaw(self.T_CONSIDERATE_MINIONS) * 20)
 	end,
 }
